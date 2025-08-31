3afc87e8afff0d46f0f0e552e9b5ef96797dd967
shujiejune <jieniu.june@tutanota.com>
2025-08-31 00:49:35 -0700

    feat: add text-to-speech feature to RenderQA

--- src/components/RenderQA.js
+++ src/components/RenderQA.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { Spin } from "antd";
+import { AudioOutlined } from "@ant-design/icons";
+import axios from "axios";
+import { Buffer } from "buffer";
+
+const DOMAIN = "http://localhost:5001";
 
 const containerStyle = {
   display: "flex",
@@ -36,10 +41,38 @@ const agentStyle = {
   borderRadius: "10px",
   padding: "10px",
   marginBottom: "10px",
+  position: "relative",
+};
+
+const audioIconStyle = {
+  position: "absolute",
+  top: "50%",
+  right: "-25px",
+  transform: "translateY(-50%)",
+  cursor: "pointer",
 };
 
 const RenderQA = (props) => {
   const { conversation } = props;
+
+  const handleTextToSpeech = async (words) => {
+    try {
+      const response = await axios.get(`${DOMAIN}/tts?words=${words}`);
+      const decodedAudioData = Buffer.from(response?.data?.audio_data, "base64");
+
+      // Create a Blob with the correct MIME type
+      const audioBlob = new Blob([decodedAudioData], { type: "audio/mp3" });
+
+      // Create an object URL from the Blob
+      const audioUrl = URL.createObjectURL(audioBlob);
+
+      // Play the audio
+      const audio = new Audio(audioUrl);
+      audio.play();
+    } catch (error) {
+      console.error("Error with TTS API: ", error);
+    }
+  };
 
   return (
     <>
@@ -50,7 +83,17 @@ const RenderQA = (props) => {
           </div>
           <div style={agentContainer}>
             <div style={agentStyle}>
-              {each.answer === null ? <Spin size="small" /> : each.answer}
+              {each.answer === null ? (
+                <Spin size="small" />
+              ) : (
+                <>
+                  {each.answer}
+                  <AudioOutlined
+                    syle={audioIconStyle}
+                    onClick={() => handleTextToSpeech(each.answer)}
+                  />
+                </>
+              )}
             </div>
           </div>
         </div>
